6e04aa6bf206350c6436c88a1f1679cd238885e8
Acly <aclysia@gmail.com>
2024-02-06 22:49:28 +0100

    Prefer python3.11 > python3.10 > python3 if found #374 - this prefers those versions even if newer ones are installed and the system default - python 3.12 is still not supported by all dependencies

--- ai_diffusion/server.py
+++ ai_diffusion/server.py
@@ -80,7 +80,7 @@ class Server:
         python_search_paths = [self.path / "python", self.path / "venv" / "bin"]
         python_path = _find_component(python_pkg, python_search_paths)
         if python_path is None:
-            self._python_cmd = _find_program("python3", "python")
+            self._python_cmd = _find_program("python3.11", "python3.10", "python3", "python")
             self._pip_cmd = _find_program("pip3", "pip")
         else:
             self._python_cmd = python_path / f"python{_exe}"
