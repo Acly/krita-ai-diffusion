0b653b104bfdbbdd9e318382b134d187f493dcf3
Acly <aclysia@gmail.com>
2024-02-07 00:02:19 +0100

    Fix path dict test on linux

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,7 @@
 import pytest
 from tempfile import TemporaryDirectory
 from pathlib import Path
+from ai_diffusion import util
 from ai_diffusion.util import batched, sanitize_prompt, find_unused_path, get_path_dict, ZipFile
 
 
@@ -28,12 +29,14 @@ def test_unused_path():
 
 
 def test_path_dict():
+    # test with mixed forward/backward slashes only works on windows
+    bs = "\\" if util.is_windows else "/"
     paths = [
         "f1.txt",
         "f2.txxt",
         "d1/f1.txt",
-        "d2\\f1.txt",
-        "d3/d4\\d5/f1.txt",
+        f"d2{bs}f1.txt",
+        f"d3/d4{bs}d5/f1.txt",
         "d1/f2.txt",
         str(Path("w1/f1.txt")),
         str(Path("w1/noext")),
@@ -43,8 +46,8 @@ def test_path_dict():
         "f1.txt": "f1.txt",
         "f2.txxt": "f2.txxt",
         "d1": {"f1.txt": "d1/f1.txt", "f2.txt": "d1/f2.txt"},
-        "d2": {"f1.txt": "d2\\f1.txt"},
-        "d3": {"d4": {"d5": {"f1.txt": "d3/d4\\d5/f1.txt"}}},
+        "d2": {"f1.txt": f"d2{bs}f1.txt"},
+        "d3": {"d4": {"d5": {"f1.txt": f"d3/d4{bs}d5/f1.txt"}}},
         "w1": {"f1.txt": paths[-3], "noext": paths[-2], "w2": {".noext": paths[-1]}},
     }
     assert get_path_dict(paths) == expected
