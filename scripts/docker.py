920c0c4af90a7cb58f3af7db6d05e2554d59e61d
Acly <aclysia@gmail.com>
2024-04-03 11:37:23 +0200

    Update docker image for 1.16.1 - moved git clone outside of docker build due to repeated connection failures...

--- scripts/docker.py
+++ scripts/docker.py
@@ -4,15 +4,39 @@ ComfyUI with all required custom nodes and models.
 
 import asyncio
 import sys
+import shutil
+import requests
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
 import ai_diffusion
+from ai_diffusion import resources
 from scripts.download_models import all_models, main as download_models
 
 version = f"v{ai_diffusion.__version__}"
 docker_root = Path(__file__).parent / "docker"
 download_folder = docker_root / "downloads"
+comfy_dir = docker_root / "ComfyUI"
+
+
+def download_repository(url: str, target: Path, revision):
+    if not target.exists():
+        archive = target.parent / f"{target.name}.zip"
+        if not url.endswith(".zip"):  # git repo URL
+            url = f"{url}/archive/{revision}.zip"
+        print("Downloading", url)
+        with open(archive, "wb") as f:
+            f.write(requests.get(url, allow_redirects=True).content)
+        shutil.unpack_archive(archive, target.parent)
+        archive.unlink()
+        shutil.move(target.parent / f"{target.name}-{revision}", target)
+
+
+def download_repositories():
+    download_repository(resources.comfy_url, comfy_dir, resources.comfy_version)
+    custom_nodes_dir = comfy_dir / "custom_nodes"
+    for repo in resources.required_custom_nodes:
+        download_repository(repo.url, custom_nodes_dir / repo.folder, repo.version)
 
 
 def clean(models):
@@ -24,6 +48,9 @@ def clean(models):
 
 
 async def main():
+    print("Downloading repositories")
+    download_repositories()
+
     print("Cleaning up cached models")
     clean(all_models())
 
