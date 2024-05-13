c948b899a38820943c32d1dbc6dcec9c3a745a6d
Acly <aclysia@gmail.com>
2024-05-13 09:49:15 +0200

    Small update to cost computation (low steps counts were too cheap relatively)

--- ai_diffusion/api.py
+++ ai_diffusion/api.py
@@ -167,8 +167,8 @@ class WorkflowInput:
             return batch * extent.pixel_count * math.sqrt(extent.pixel_count) * steps
 
         base = 1 if ensure(self.models).version is SDVersion.sd15 else 2
-        steps = ensure(self.sampling).actual_steps
-        unit = cost_factor(2, Extent(1024, 1024), 20)
+        steps = max(8, ensure(self.sampling).actual_steps)
+        unit = cost_factor(2, Extent(1024, 1024), 24)
         cost = cost_factor(self.batch_count, self.extent.desired, steps)
         return base + round((10 * cost) / unit)
 
