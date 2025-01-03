c85cbffcb8df490761d5c3d361ed745fba5c82ad
Acly <aclysia@gmail.com>
2025-01-03 20:44:22 +0100

    Create documentation pages with starlight

---
+++ docs/astro.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://docs.interstice.cloud',
+	integrations: [
+		starlight({
+			title: 'Krita Diffusion Plugin',
+			logo: { src: './src/assets/logo.png' },
+			favicon: '/favicon.png',
+			social: {
+				github: 'https://github.com/Acly/krita-ai-diffusion',
+				discord: 'https://discord.gg/pWyzHfHHhU',
+				youtube: 'https://www.youtube.com/@aclysia',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						{ label: 'Regions', slug: 'regions' },
+					],
+				},
+			],
+			customCss: ['./src/styles/custom.css'],
+		}),
+		icon()],
+});
