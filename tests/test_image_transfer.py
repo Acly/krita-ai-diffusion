ff5b5b965a7253e42c025ab0db4925d6dd3683d5
Acly <aclysia@gmail.com>
2025-01-21 10:20:17 +0100

    Adapt image transfer test

--- tests/test_image_transfer.py
+++ tests/test_image_transfer.py
@@ -1,5 +1,6 @@
 from base64 import b64decode
 from PIL import Image
+from datetime import datetime
 import os
 import pytest
 
@@ -13,6 +14,7 @@ if (root_dir / "service" / "pod" / "lib").exists():
 
     dotenv.load_dotenv(root_dir / "service" / "web" / ".env.local")
     from service.pod.lib import image_transfer
+    from service.pod.lib import log
 
     max_b64_size_config = {
         "transfer": 100_000,  # use R2 for images > 100kb -> will use R2
@@ -28,7 +30,10 @@ if (root_dir / "service" / "pod" / "lib").exists():
         max_b64_size = max_b64_size_config[mode]
 
         async def main():
-            transfer = await image_transfer.send_images(images, max_inline_size=max_b64_size)
+            metrics = log.Metrics("test", datetime.now())
+            transfer = await image_transfer.send_images(
+                images, metrics, max_inline_size=max_b64_size
+            )
             assert len(transfer["offsets"]) == 2
 
             if mode == "transfer":
