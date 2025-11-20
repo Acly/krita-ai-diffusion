fdedbd76601a7b8647a8a4e09f759ee6fe219a28
Acly <aclysia@gmail.com>
2025-11-20 12:06:12 +0100

    Don't try connecting repeatedly if the server mode has changed

--- ai_diffusion/root.py
+++ ai_diffusion/root.py
@@ -131,7 +131,11 @@ class Root(QObject):
                     if connection.state is ConnectionState.connected:
                         settings.server_url = url
                         break
-                    elif connection.error_kind != "network" or urls[0] != settings.server_url:
+                    elif (
+                        connection.error_kind != "network"
+                        or urls[0] != settings.server_url
+                        or settings.server_mode not in [ServerMode.undefined, ServerMode.external]
+                    ):
                         break
                     await asyncio.sleep(5 * (retry + 1))
                 if settings.server_mode is ServerMode.undefined:
