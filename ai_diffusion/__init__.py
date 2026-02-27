2eab2f8072bf59b7be7e7c0c7f252b5b6630d320
Acly <aclysia@gmail.com>
2026-02-27 14:19:31 +0100

    Add qwen-3-vl to file server (dev only)

--- ai_diffusion/__init__.py
+++ ai_diffusion/__init__.py
@@ -1,6 +1,6 @@
 """Generative AI plugin for Krita"""
 
-__version__ = "1.48.0"
+__version__ = "1.49.0"
 
 import importlib.util
 
--- scripts/file_server.py
+++ scripts/file_server.py
@@ -13,6 +13,7 @@ from aiohttp import web
 
 sys.path.append(str(Path(__file__).parent.parent))
 from ai_diffusion import resources
+from ai_diffusion.resources import Arch, ModelFile, ModelResource, ResourceId, ResourceKind
 
 dir = Path(__file__).parent / "downloads"
 
@@ -22,15 +23,29 @@ def url_strip(url: str):
     return without_host.split("?", 1)[0]
 
 
-files = {
-    url_unquote(url_strip(file.url)): dir / file.path
-    for m in resources.all_models(include_deprecated=True)
-    for file in m.files
-}
-
-urls = {
-    url_strip(file.url) for m in resources.all_models(include_deprecated=True) for file in m.files
-}
+all_models = list(resources.all_models(include_deprecated=True))
+all_models.append(
+    ModelResource(
+        name="Qwen3-VL-4B-Instruct",
+        id=ResourceId(ResourceKind.checkpoint, Arch.qwen, "qwen3-vl-4b-instruct"),
+        files=[
+            ModelFile(
+                Path("llm/Qwen3-VL-4B-Instruct-IQ4_XS.gguf"),
+                "https://huggingface.co/unsloth/Qwen3-VL-4B-Instruct-GGUF/resolve/main/Qwen3-VL-4B-Instruct-IQ4_XS.gguf",
+                ResourceId(ResourceKind.checkpoint, Arch.qwen, "qwen3-vl-4b-instruct"),
+                "011018e3aba055a9250f9fb11efdeebfb6805df767922bfdb91607fce866416c",
+            ),
+            ModelFile(
+                Path("llm/mmproj-Qwen3-VL-4B-Instruct-BF16.gguf"),
+                "https://huggingface.co/unsloth/Qwen3-VL-4B-Instruct-GGUF/resolve/main/mmproj-BF16.gguf",
+                ResourceId(ResourceKind.clip_vision, Arch.qwen, "qwen3-vl-4b-instruct"),
+                "364ef9a7502b7e9a834b9c013b953670f6ba2550b4a26fc043c317fa484eb53e",
+            ),
+        ],
+    )
+)
+files = {url_unquote(url_strip(file.url)): dir / file.path for m in all_models for file in m.files}
+urls = {url_strip(file.url) for m in all_models for file in m.files}
 
 
 async def file_sender(file: Path):
